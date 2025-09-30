dac9cd21d5b2d1081b045966de83b13b866b9c3f
Aditi Bhadoriya <awdhesh1700@gmail.com>
2025-09-30 15:20:06 +0530

    changed model from 1.5 to 2.5 gemini flash prev got deprecated

--- src/utils/generateAudienceInsights.js
+++ src/utils/generateAudienceInsights.js
@@ -7,7 +7,8 @@ const generateAudienceInsights = async (demographicData) => {
     }
 
     const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    // const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const prompt = `
 You are an audience strategist helping creators present their audience strengths to potential brand partners.
--- src/utils/generateFormDataFromUserInput.js
+++ src/utils/generateFormDataFromUserInput.js
@@ -7,7 +7,9 @@ export const generateFormDataFromUserInput = async (userInput, isBrandCollaborat
   }
 
   const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  // const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+
 
     const basePrompt = `
 
