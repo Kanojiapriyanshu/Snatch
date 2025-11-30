650e28d7075a135776fa3d62614292eaa4a51ce5
Aditi Bhadoriya <awdhesh1700@gmail.com>
2025-11-30 19:51:56 +0530

    added force-dynamic in /api/inngest to have recurring cron jobs

--- src/app/api/inngest/route.js
+++ src/app/api/inngest/route.js
@@ -1,9 +1,10 @@
 //src/app/api/inngest/route.js
-export const runtime = "nodejs";
+export const dynamic = 'force-dynamic'
 
 import { serve } from "inngest/next";
 import { inngest } from "../../../../inngest/client.js";
 import { refreshInstagramMedia } from "../../../../inngest/functions/refreshInstagramMedia.js";
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [refreshInstagramMedia],
